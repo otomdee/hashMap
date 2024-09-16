4ccd9bf449093f8b9eaa0064a6a9638e4a4f4da3
otomdee <otomdaniel65@gmail.com>
2024-09-16 21:02:37 +0100

    add hash function

---
+++ hashMap.js
@@ -0,0 +1,16 @@
+class HashMap {
+    constructor() {
+        this.buckets = [];
+    }
+}
+
+function hash(key) {
+    let hashCode = 0;
+       
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+ 
+    return hashCode;
+  } 
